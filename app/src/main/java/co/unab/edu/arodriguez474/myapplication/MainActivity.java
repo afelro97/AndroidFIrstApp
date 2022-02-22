1e31a1f6502a5d383a5dde4f16ce3febb5289a29
afelro97 <afelro97@gmail.com>
2022-02-22 15:11:41 -0500

    Logs Added on main activity

--- app/src/main/java/co/unab/edu/arodriguez474/myapplication/MainActivity.java
+++ app/src/main/java/co/unab/edu/arodriguez474/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package co.unab.edu.arodriguez474.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("ciclo_vida", "Ejecutando Oncreate");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("ciclo_vida", "Ejecutando onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ciclo_vida", "Ejecutando onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ciclo_vida", "Ejecutando onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("ciclo_vida", "Ejecutando onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("ciclo_vida", "Ejecutando onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("ciclo_vida", "Ejecutando onRestart");
     }
 }
